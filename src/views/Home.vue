b2bec3a0f448afb58dc15086dabccce2495e3fa1
Nomusa Mtshali <kelebogilemataboge17@gmail.com git config --global user.email kelebogilemataboge17@gmail.com>
2024-08-20 16:27:34 +0200

    changed style of the carousel

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,15 @@
 <script setup>
-import { ref, computed, onMounted } from 'vue';
-import ProductCard from '../components/ProductCard.vue';
-import { useStore } from '../store';
+import { ref, computed, onMounted } from "vue";
+import ProductCard from "../components/ProductCard.vue";
+import { useStore } from "../store";
 
 const products = ref([]);
 const categories = ref([]);
-const selectedCategory = ref('');
-const selectedSort = ref('');
+const selectedCategory = ref("");
+const selectedSort = ref("");
 const store = useStore();
+
+const currentIndex = ref(0);
 
 // Fetch products and categories on component mount
 onMounted(async () => {
@@ -16,7 +18,9 @@ onMounted(async () => {
   products.value = data;
 
   // Extract unique categories
-  categories.value = [...new Set(products.value.map(product => product.category))];
+  categories.value = [
+    ...new Set(products.value.map((product) => product.category)),
+  ];
 });
 
 // Filter and sort products
@@ -24,12 +28,14 @@ const filteredAndSortedProducts = computed(() => {
   let filteredProducts = products.value;
 
   if (selectedCategory.value) {
-    filteredProducts = filteredProducts.filter(product => product.category === selectedCategory.value);
+    filteredProducts = filteredProducts.filter(
+      (product) => product.category === selectedCategory.value
+    );
   }
 
-  if (selectedSort.value === 'lowest') {
+  if (selectedSort.value === "lowest") {
     filteredProducts = filteredProducts.sort((a, b) => a.price - b.price);
-  } else if (selectedSort.value === 'highest') {
+  } else if (selectedSort.value === "highest") {
     filteredProducts = filteredProducts.sort((a, b) => b.price - a.price);
   }
 
@@ -39,62 +45,152 @@ const filteredAndSortedProducts = computed(() => {
 // Wishlist carousel
 const wishlistItems = computed(() => store.getWishlist());
 
+const nextItem = () => {
+  if (currentIndex.value < wishlistItems.value.length - 1) {
+    currentIndex.value++;
+  }
+};
+
+const prevItem = () => {
+  if (currentIndex.value > 0) {
+    currentIndex.value--;
+  }
+};
 </script>
 
 <template>
-<!-- Wishlist Carousel -->
-    <div v-if="wishlistItems.length > 0" class="mt-">
-      <h2 class="text-xl font-bold mb-2">My Wishlist:</h2>
-      <div class="carousel-container overflow-x-auto whitespace-nowrap space-x-4">
-        <div v-for="item in wishlistItems" :key="item.id" class="inline-block product-card border rounded-lg shadow-md bg-white dark:bg-gray-800 mx-auto" style="width: 15rem;">
-          <img :src="item.image" :alt="item.title" class="product-image w-full object-contain p-2" />
+  <div class="container mx-auto py-6">
+    <!-- Wishlist Carousel -->
+    <div v-if="wishlistItems.length > 0" class="mt-12 relative">
+      <h2 class="text-xl font-bold mb-2 text-center">My Wishlist</h2>
+      <div class="flex items-center justify-center space-x-4">
+        <!-- Left Arrow -->
+        <button
+          @click="prevItem"
+          :disabled="currentIndex === 0"
+          class="p-2 text-gray-800 dark:text-gray-300 hover:text-gray-500 dark:hover:text-gray-500 disabled:opacity-50"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-8 w-8"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M15 19l-7-7 7-7"
+            />
+          </svg>
+        </button>
+
+        <!-- Carousel Item -->
+        <div
+          class="product-card border rounded-lg shadow-md bg-white dark:bg-gray-800 mx-4"
+          style="width: 15rem"
+        >
+          <img
+            :src="wishlistItems[currentIndex].image"
+            :alt="wishlistItems[currentIndex].title"
+            class="product-image w-full object-contain p-2"
+          />
           <div class="px-4 pb-4">
-            <h2 class="text-md font-semibold text-gray-800 dark:text-gray-200 mb-2 truncate">{{ item.title }}</h2>
-            <p class="text-sm text-gray-500 dark:text-gray-400 mb-2">${{ item.price }}</p>
-            <router-link :to="`/product/${item.id}`" class="block text-center text-white dark:text-gray-900 bg-teal-500 dark:bg-teal-300 hover:bg-teal-600 dark:hover:bg-teal-400 font-semibold rounded-md py-2 mb-2 transition-colors duration-200 ease-in-out">
+            <h2
+              class="text-md font-semibold text-gray-800 dark:text-gray-200 mb-2 truncate"
+            >
+              {{ wishlistItems[currentIndex].title }}
+            </h2>
+            <p class="text-sm text-gray-500 dark:text-gray-400 mb-2">
+              ${{ wishlistItems[currentIndex].price }}
+            </p>
+            <router-link
+              :to="`/product/${wishlistItems[currentIndex].id}`"
+              class="block text-center text-white dark:text-gray-900 bg-teal-500 dark:bg-teal-300 hover:bg-teal-600 dark:hover:bg-teal-400 font-semibold rounded-md py-2 mb-2 transition-colors duration-200 ease-in-out"
+            >
               View Details
             </router-link>
           </div>
         </div>
+
+        <!-- Right Arrow -->
+        <button
+          @click="nextItem"
+          :disabled="currentIndex === wishlistItems.length - 1"
+          class="p-2 text-gray-800 dark:text-gray-300 hover:text-gray-500 dark:hover:text-gray-500 disabled:opacity-50"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-8 w-8"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M9 5l7 7-7 7"
+            />
+          </svg>
+        </button>
       </div>
     </div>
 
-  <div class="container mx-auto py-6">
-    <!-- Filter and Sort Controls -->
-    <div class="flex flex-wrap justify-center mb-8">
-      <div>
-        <label for="category" class="mr-2 font-semibold">Filter by Category:</label>
-        <select id="category" v-model="selectedCategory" @change="filterProducts" class="p-2 border rounded-md">
-          <option value="">All Categories</option>
-          <option v-for="category in categories" :key="category" :value="category">{{ category }}</option>
-        </select>
+    <div class="container mx-auto py-6">
+      <!-- Filter and Sort Controls -->
+      <div class="flex flex-wrap justify-center mb-8">
+        <div>
+          <label for="category" class="mr-2 font-semibold"
+            >Filter by Category:</label
+          >
+          <select
+            id="category"
+            v-model="selectedCategory"
+            @change="filterProducts"
+            class="p-2 border rounded-md"
+          >
+            <option value="">All Categories</option>
+            <option
+              v-for="category in categories"
+              :key="category"
+              :value="category"
+            >
+              {{ category }}
+            </option>
+          </select>
+        </div>
+        <div class="ml-12">
+          <label for="sort" class="mr-2 font-semibold">Sort by Price:</label>
+          <select
+            id="sort"
+            v-model="selectedSort"
+            @change="sortProducts"
+            class="p-2 border rounded-md"
+          >
+            <option value="">Default</option>
+            <option value="lowest">Lowest to Highest</option>
+            <option value="highest">Highest to Lowest</option>
+          </select>
+        </div>
       </div>
-      <div class="ml-12">
-        <label for="sort" class="mr-2 font-semibold">Sort by Price:</label>
-        <select id="sort" v-model="selectedSort" @change="sortProducts" class="p-2 border rounded-md">
-          <option value="">Default</option>
-          <option value="lowest">Lowest to Highest</option>
-          <option value="highest">Highest to Lowest</option>
-        </select>
+
+      <!-- Product Grid -->
+      <div
+        class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6"
+      >
+        <ProductCard
+          v-for="product in filteredAndSortedProducts"
+          :key="product.id"
+          :product="product"
+        />
       </div>
     </div>
-
-    <!-- Product Grid -->
-    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-      <ProductCard v-for="product in filteredAndSortedProducts" :key="product.id" :product="product" />
-    </div>
-
-    
   </div>
 </template>
 
 <style scoped>
-.carousel-container {
-  display: flex;
-  overflow-x: auto;
-  padding: 1rem 0;
-}
-
 .product-card {
   display: flex;
   flex-direction: column;
@@ -106,22 +202,13 @@ const wishlistItems = computed(() => store.getWishlist());
   border-bottom: 1px solid #e5e7eb;
 }
 
-.product-card .px-4 {
-  padding-left: 1rem;
-  padding-right: 1rem;
-}
-
-.product-card .pb-4 {
-  padding-bottom: 1rem;
-}
-
 .product-card .truncate {
   white-space: nowrap;
   overflow: hidden;
   text-overflow: ellipsis;
 }
 
-.carousel-container > .product-card + .product-card {
-  margin-left: 1rem;
+button:disabled {
+  cursor: not-allowed;
 }
 </style>
