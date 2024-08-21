9967e11eeb1742ca49367faa2d4bac1a37a6e2cf
Nomusa Mtshali <kelebogilemataboge17@gmail.com git config --global user.email kelebogilemataboge17@gmail.com>
2024-08-21 10:30:34 +0200

    implemented a notification component that'll display the notifications/messages when different actions are performed

---
+++ src/components/Notification.vue
@@ -0,0 +1,28 @@
+<!-- src/components/Notification.vue -->
+<template>
+    <transition name="fade">
+      <div v-if="message" class="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50">
+        <span class="text-white bg-red-500 px-4 py-2 rounded-md">
+          {{ message }}
+        </span>
+      </div>
+    </transition>
+  </template>
+  
+  <script setup>
+  import { computed } from 'vue';
+  import { useStore } from '../store';
+  
+  const store = useStore();
+  const message = computed(() => store.notificationMessage);
+  </script>
+  
+  <style scoped>
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity 0.5s;
+  }
+  .fade-enter-from, .fade-leave-to {
+    opacity: 0;
+  }
+  </style>
+  
